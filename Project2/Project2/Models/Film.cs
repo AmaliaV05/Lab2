41d4f778c8070c0b0f9101f82a8f353bca4c418a
Amalia Vargatu <avargatu@yahoo.com>
2021-05-28 18:02:08 +0300

    add film validation

--- Project2/Project2/Models/Film.cs
+++ Project2/Project2/Models/Film.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,13 +9,18 @@ namespace Project2.Models
     public class Film
     {
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
         public enum Genre { Action, Comedy, Horror, Thriller }
+        [Required]
         public string Duration { get; set; }
         public int YearOfRelease { get; set; }
+        [Required]
         public string Director { get; set; }
         public DateTime DateAdded { get; set; }
+        [Range(1,10)]
         public int Rating { get; set; }
+        [Required]
         public string Watched { get; set; }
     }
 }
