8abc42df8361fca9b268d39c6cc4127de4782c74
Amalia Vargatu <avargatu@yahoo.com>
2021-07-12 13:09:11 +0300

    update to angular 11

--- Project2/Project2/ClientApp/src/app/films/films.component.spec.ts
+++ Project2/Project2/ClientApp/src/app/films/films.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { FilmsComponent } from './films.component';
 
@@ -6,7 +6,7 @@ describe('FilmsComponent', () => {
   let component: FilmsComponent;
   let fixture: ComponentFixture<FilmsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ FilmsComponent ]
     })
